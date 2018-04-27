049176c95ee80809548de8c3ddcb65bd31d073bc
Michael Chartoire <chartoire.michael@neuf.fr>
2018-04-27 13:58:50 +0200

    dernieres modifs

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,12 +1,53 @@
+
 public class BowlingGame {
 
-    private int score;
+    int tour[][];
+    int nbQuilles = 0;
+    int lanceUn = 0;
+    int lanceDeux = 0;
+    int indexTour;
+    int indexLance = 1;
+    int lanceBonus;
+    int score = 0;
 
-    public int score() {
-        return score;
+    // methode pour r�cup�rer le score.
+    public int scoreAdd() {
+	score = score + tour[indexTour][indexLance] + tour[indexTour][indexLance++]; // Valeur que l'on va ajouter à
+										     // chaque tour.
+	return score;
     }
 
-    public void roll(int numberPins) {
-        score += numberPins;
+    // R�cup�rer les bons bonus
+    public int bonusQuilles() {
+
+	for (indexTour = 1; indexTour <= 10; indexTour++) {
+
+	    if (nbQuilles == 10) {
+		// SPARE
+		if (tour[indexTour][indexLance] + tour[indexTour][indexLance++] == 10) { // lancé 1 et lancé 2 de
+											 // l'index courant == 10
+		    indexTour++; // on incr�mente pour passer au tour suivant et recuperer la valeur du premier
+				 // lanc�
+		    lanceBonus = tour[indexTour][indexLance]; // premier lance du tour suivant
+		} else if (tour[indexTour][indexLance] == 10) {
+		    // STRIKE
+		    // tour[indexTour][indexLance++] = int.parseInt(toString( ));
+		    tour[indexTour][indexLance++] = 0;
+		    // essai de conversion de la valeur int en string pour n'avoir rien à afficher
+		    indexTour++;
+		    lanceBonus = tour[indexTour][indexLance] + tour[indexTour][indexLance++];
+		} else {
+		    lanceBonus = 0;
+		}
+		// soit strike soit spare
+	    }
+	}
+	return lanceBonus;
     }
 }
+/*
+ * 10 quilles par tour, 2 lancés par tour si lancé 1 == 10, strike donc bonus
+ * strike si lancé 1 + lancé 2 == 10, spare donc bonus spare si lancé 1 +
+ * lancé 2 < 10, pas bonus si tour 10 et strike, 2 lancés suppl si tour 10 et
+ * spare, 1 lancé suppl
+ */
--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,7 +1,5 @@
+
 import org.junit.Test;
-
-import static junit.framework.TestCase.assertEquals;
-
 
 public class BowlingGameTest {
 
@@ -9,29 +7,53 @@ public class BowlingGameTest {
 
     @Test
     public void queDesGoutieresDonne0Points() {
-        rollMany(20, 0);
-        assertEquals(0, game.score());
+	rollMany(20, 0);
+	assertEquals(0, game.score());
     }
 
     @Test
-    public void uneSeuleQuille(){
-        game.roll(1);
-        rollMany(19, 0);
-        assertEquals(1, game.score());
+    public void uneSeuleQuille() {
+	game.roll(1);
+	rollMany(19, 0);
+	assertEquals(1, game.score());
     }
 
     @Test
-    public void bonusDuSpare(){
-        game.roll(8);
-        game.roll(2);
-        game.roll(1);
-        rollMany(17, 0);
-        assertEquals(12, game.score());
+    public void bonusDuSpare() {
+	game.roll(8);
+	game.roll(2);
+	game.roll(4);
+	// game.roll(4);
+	// le 3e lancé vaut 2 fois sa valeur
+	rollMany(17, 0);
+	assertEquals(18, game.score());
     }
 
-    private void rollMany(int numberRolls, int numberPins) {
-        for (int i = 0; i < numberRolls; i++) {
-            game.roll(numberPins);
-        }
+    @Test
+    public void bonusDuStrick() {
+	game.roll(10);
+	game.roll(4);
+	game.roll(3);
+	// game.roll(4);
+	// game.roll(3);
+	rollMany(16, 0);
+	assertEquals(24, game.score());
     }
+
+    @Test
+    public void bonusPartieParfaite() {
+	rollMany(10, 30);
+
+	/*
+	 * game.roll(10); game.roll(10); game.roll(10); /rollMany()
+	 */
+	assertEquals(300, game.score());
+    }
+
+    private void rollMany(int indexTour, int nbQuilles) {
+	for (int i = 0; i < indexTour; i++) {
+	    game.roll(nbQuilles);
+	}
+    }
+
 }
